642eee0815e9837ac958c44b94e411b751365d24
Higor Luiz <higorrluiz@gmail.com>
2023-09-27 20:24:09 -0300

    tela de detalhes da turma

---
+++ lib/telas/inicio/detalhes_lista.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+
+import '../../modelos/attendance.dart';
+import '../../modelos/classroom.dart';
+
+class ClassroomDetailScreen extends StatefulWidget {
+  final Classroom classroom;
+  ClassroomDetailScreen({Key? key, required this.classroom}) : super(key: key);
+  bool autoAttendance = false;
+
+  @override
+  _ClassroomDetailScreenState createState() => _ClassroomDetailScreenState();
+}
+
+class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
+  bool _isCollapsed = true;
+  Attendance attendance = Attendance();
+  List<String> weekDays = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+  String selectedWeekDay = '';
+  bool aluno = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Informações da Turma'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0, bottom: 20.0),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(widget.classroom.name, style: const TextStyle(fontSize: 20)),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(widget.classroom.classCode, style: const TextStyle(fontSize: 16)),
+                    Text(widget.classroom.classTime, style: const TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Navegue para a página de histórico da turma
+                  // Substitua '/historicoTurma' pela rota correta
+                  Navigator.of(context).pushNamed('/historicoTurma');
+                },
+                child: Text('Histórico da Turma'),
+              ),
+              ListTile(
+                title: const Text('Configurações de Chamada', style: TextStyle(fontSize: 24)),
+              ),
+              Container(
+                padding: EdgeInsets.all(0.5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 3,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(_isCollapsed ? 'Manual' : 'Automática', style: TextStyle(fontSize: 16)),
+                  onTap: () {
+                    setState(() {
+                      _isCollapsed = !_isCollapsed;
+                    });
+                  },
+                  trailing: Icon(
+                    _isCollapsed ? Icons.expand_more : Icons.expand_less,
+                  ),
+                ),
+              ),
+              if (_isCollapsed && aluno == false) ...[
+                ListTile(
+                  title: Text('Iniciar chamada'),
+                  trailing: Switch(
+                    value: widget.autoAttendance,
+                    onChanged: (value) {
+                      setState(() {
+                        widget.autoAttendance = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+              if(aluno == true) ...[
+                SizedBox(height: 50,),
+                ElevatedButton(onPressed: (){}, 
+                  child: Text("Marcar Presença"),
+                  style: ElevatedButton.styleFrom(minimumSize: Size(200,80))
+                ),
+              ],
+              if (!_isCollapsed) ...[
+                ListTile(
+                  title: Text('Dia da Chamada', style: TextStyle(fontSize: 20)),
+                ),
+                ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: weekDays.length,
+                  separatorBuilder: (context, index) {
+                    return Divider(height: 1, color: Colors.grey);
+                  },
+                  itemBuilder: (context, index) {
+                    final weekDay = weekDays[index];
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedWeekDay = weekDay;
+                        });
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 3,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(weekDay),
+                              if (selectedWeekDay == weekDay)
+                                Icon(Icons.check_circle, color: Colors.green),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0), // Adicione o padding apenas na parte inferior
+                    child: Text('Horário da Chamada', style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ListTile(
+                  title: Container(
+                    padding: EdgeInsets.all(1.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: GestureDetector(
+                        onTap: () async {
+                          TimeOfDay? selectedTime = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.now(),
+                          );
+
+                          if (selectedTime != null) {
+                            String formattedTime = selectedTime.format(context);
+                            setState(() {
+                              attendance.start = formattedTime;
+                            });
+                          }
+                        },
+                        child: Row(
+                          children: [
+                            Icon(Icons.access_time), // Ícone de relógio
+                            SizedBox(width: 8.0),
+                            Text(
+                              attendance.start,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/turmas/item.dart
+++ lib/widgets/turmas/item.dart
@@ -1,5 +1,7 @@
+import 'package:chamada_inteligente/helpers/salas_de_aula_dummy.dart';
 import 'package:flutter/material.dart';
 import '../../modelos/classroom.dart';
+import '../../telas/inicio/detalhes_lista.dart';
 
 class ClassItem extends StatelessWidget {
   final Classroom classroom;
@@ -9,38 +11,50 @@ class ClassItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 7.0, left: 10.0, right: 10.0, bottom: 7.0),
+      padding:
+          const EdgeInsets.only(top: 7.0, left: 10.0, right: 10.0, bottom: 7.0),
       child: Container(
-          decoration: BoxDecoration(
+        decoration: BoxDecoration(
           color: Colors.white, // Cor de fundo do retângulo branco
-          borderRadius: BorderRadius.all(Radius.circular(8.0)), // Borda arredondada
+          borderRadius:
+              BorderRadius.all(Radius.circular(8.0)), // Borda arredondada
           boxShadow: [
-          BoxShadow(
-          color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 2,
-          blurRadius: 3,
-          offset: Offset(0, 2),
-          ),
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 2),
+            ),
           ],
         ),
         child: ListTile(
-          title: Text(classroom.name,
+          title: Text(
+            classroom.name,
             style: TextStyle(fontSize: 20),
           ),
           subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(classroom.classCode,
-                style: TextStyle(fontSize: 16),),
-              Text(classroom.classTime,
-                style: TextStyle(fontSize: 16),),
+              Text(
+                classroom.classCode,
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                classroom.classTime,
+                style: TextStyle(fontSize: 16),
+              ),
             ],
           ),
-          trailing:
-            classroom.activeClass ? Icon(Icons.check_circle, color: Colors.green) : null,
+          trailing: classroom.activeClass
+              ? Icon(Icons.check_circle, color: Colors.green)
+              : null,
           onTap: () {
-            // Navigator.of(context).pushNamed('/classDetail', arguments: classroom);
-            },
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        ClassroomDetailScreen(classroom:classroom)));
+          },
         ),
       ),
     );
